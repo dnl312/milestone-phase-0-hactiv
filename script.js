177faa267a57a85187f529ea7df00e166f91c816
dnl312 <48292382+dnl312@users.noreply.github.com>
2024-10-23 01:55:22 +0700

    add mudic, interactive school page, gallery

---
+++ script.js
@@ -0,0 +1,16 @@
+const audioPlayer = document.getElementById("audioPlayer");
+
+// Restore the last position of the audio
+window.addEventListener("load", () => {
+  const lastTime = localStorage.getItem("audioTime");
+  if (lastTime) {
+    audioPlayer.currentTime = lastTime;
+    audioPlayer.volume = 0.4;
+    audioPlayer.play();
+  }
+});
+
+// Save the current position of the audio when the page is unloaded
+window.addEventListener("beforeunload", () => {
+  localStorage.setItem("audioTime", audioPlayer.currentTime);
+});
